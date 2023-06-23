e98993424718a7c35ac6871349dac27b59a125b8
martin <martinrus@wanadoo.fr>
2023-06-23 10:39:54 -0700

    news

---
+++ index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Navigation</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="apple-touch-icon" sizes="180x180" href="apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="favicon-16x16.png">
+    <link rel="manifest" href="manifest-for-js-tools.json">
+      <link rel="stylesheet" href="index.css" />
+      <script src="https://kit.fontawesome.com/ae5e25ad8f.js" crossorigin="anonymous"></script>
+      <style>
+          .overlay {
+              width: 100%;
+          }
+      </style>
+      <!-- Global site tag (gtag.js) - Google Analytics -->
+      <script async src="https://www.googletagmanager.com/gtag/js?id=G-T748YKXBMS"></script>
+      <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-T748YKXBMS');
+      </script>
+  </head>
+  <body>
+    
+    <!-- Overlay content -->
+      <div id="myNav" class="overlay">
+  <!--  <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a> -->
+  <div class="overlay-content">
+    <a href="us-road-trip-tools.html">US road trip tools</a>
+    <a href="tip-calculator.html">Tip calculator</a>
+    <a href="currency-converter.html">Currency converter</a>
+    <a href="wage-calculator.html">Wage calculator</a>
+    <a href="resume.html">Resume</a>
+    <a href="before-after.html">Van Build</a>
+    <a href="new-site/public_html/index.html" target="_blank">New porfolio</a>
+    <a href="google-ads.html" target="_blank">Ad tool</a>
+    <a href="pie chart/pie.html" target="_blank">Work schedule</a> 
+  </div>
+          
+</div>
+<footer>
+    <p>Design by <a href="https://martinruscade.com" target=_blank> martinruscade.com</a></p>
+    <div class="social">
+        <a href="https://github.com/Rusmartin" target="_blank"><i class="fa-brands fa-github"></i></a>
+        <a href="https://twitter.com/MartinRUSCADE" target="_blank"><i class="fa-brands fa-twitter"></i></a>
+        <a href="https://www.instagram.com/martin_ruscade/" target="_blank"><i class="fa-brands fa-instagram"></i></a>
+    </div>
+</footer>
+<?php include 'footer.php';?>
+  </body>
+</html>
