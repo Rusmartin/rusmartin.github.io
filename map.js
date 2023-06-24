218593241f3a60140070bceca3c735823e8478af
martin <martinrus@wanadoo.fr>
2023-06-24 13:59:28 -0700

    major update to nav overlay
    -addition of new links to nav overlay
    -creation of new page
         -europe summer travel plan

---
+++ map.js
@@ -0,0 +1,83 @@
+var statesData = {
+    "type": "FeatureCollection",
+    "features": [{
+        "type": "Feature",
+        "id": "01",
+        "properties": {
+            "name": "France",
+            "days": 20,
+            "density": 'Martin: Aug 31st to Sept 20th',
+            "Lauren": 'Lauren: Sept 12th to Sept 20th'
+        },
+        "geometry": {
+            "type": "Polygon",
+            "coordinates": [[[2.330566, 51.049628], [8.219238, 48.945538], [6.372070, 46.604167], [ 7.647949, 44.112770], [6.514893, 43.181147], [4.053955, 43.588349], [3.151523, 42.404700], [-1.597739, 43.242702], [-1.230469, 46.141783], [-4.675965, 48.383162]]]
+        }
+    }, {
+        "type": "Feature",
+        "id": "04",
+        "properties": {
+            "name": "Ireland",
+            "days": 20,
+            "density": 'Martin: Aug 13th to Aug 24th',
+            "Lauren": 'Lauren: Aug 13th to Sept 24th, 30th to Sept 12th'
+        },
+        "geometry": {
+            "type": "Polygon",
+            "coordinates": [[[-5.998535, 52.948638], [-6.339111, 52.194140], [-9.497681, 51.423189], [-10.491943, 52.200874], [-9.261475, 53.143476], [-10.228271, 53.488046], [-10.079956, 54.326135], [-8.552856, 54.351754], [-8.811035, 54.686534], [-8.311157, 55.169456], [-6.970825, 55.326019], [-8.157349, 54.431713], [-6.240234, 54.091617]]]
+        }
+    }, {
+        "type": "Feature",
+        "id": "05",
+        "properties": {
+            "name": "Spain",
+            "days": 6,
+            "density": 'Martin: Aug 24th to Aug 30th',
+            "Lauren": 'Lauren: Aug 24th to Sept 30th'
+        },
+        "geometry": {
+            "type": "Polygon",
+            "coordinates": [[[-8.876953, 43.771094], [-1.691895, 43.309191], [3.383789, 42.374778], [0.087891, 39.842286], [0.241699, 38.736946], [-1.296387, 36.756490], [-5.493164, 36.173357], [-7.404785, 37.230328], [-6.372070, 41.951320], [-9.052734, 41.934977]]]
+        }
+    }, {
+        "type": "Feature",
+        "id": "06",
+        "properties": {
+            "name": "Portugal",
+            "days": 7,
+            "density": 'Martin: Sept 27th to Oct 7th',
+            "Lauren": 'Lauren: Sept 30th to Oct 5th'
+        },
+        "geometry": {
+            "type": "Polygon",
+            "coordinates": [[[-7.404785, 37.230328], [-6.372070, 41.951320], [-9.052734, 41.934977], [-9.536133, 38.702659], [-8.986816, 37.020098]]]
+        }
+    }, {
+        "type": "Feature",
+        "id": "72",
+        "properties": {
+            "name": "Scotland",
+            "days": 2,
+            "density": 'Martin: Sept 25th to Sept 27th',
+            "Lauren": 'Lauren: Sept 25th to Sept 27th'
+        },
+        "geometry": {
+            "type": "Polygon",
+            "coordinates": [[[-1.994019, 55.776573], [-3.087158, 54.961848], [-5.119629, 54.816514], [-8.195801, 58.008098], [-2.504883, 58.836490]]]
+        }
+    },
+    {
+        "type": "Feature",
+        "id": "72",
+        "properties": {
+            "name": "England",
+            "days": 6,
+            "density": 'Martin: Sept 24th to Sept 25th',
+            "Lauren": 'Lauren: Sept 24th to Sept 25th, Sept 27th to Sept 30th,'
+        },
+        "geometry": {
+            "type": "Polygon",
+            "coordinates": [[[-1.994019, 55.776573], [-3.087158, 54.961848], [-2.925110, 53.179704], [-2.691650, 51.559997], [-3.048706, 51.206883], [-4.210510, 51.203442], [-5.748596, 50.102964], [1.411743, 51.165567], [1.713867, 52.746269]]]
+        }
+    }]
+};
